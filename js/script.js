77f49bf58dbaffbb9178ffdb82059a15e8580526
George Lam <geo.lam@hotmail.com>
2019-05-21 02:34:32 -0700

    done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,71 @@
-var list = ["Hello"];
+// Stringified JSON object that holds initial messages
+var jsonString = '{"messages":["Welcome to Post It!","You can type a message and post it to the website.","You can also clear all previous posts by clicking on Clear."]}'
+// Id for new message in list (for remove function)
+var msgId = 1;
 
-document.getElementById("test").innerHTML = "New text!"; 
+function start() {
+  var json = JSON.parse(jsonString);
+  var initialList = json.messages;
+
+  var length = initialList.length;
+  for (var i = 0; i < length; i++) {
+    // Create list element
+    var listElement = document.createElement('li');
+    listElement.setAttribute("id", msgId);
+
+    // Create new div and add new paragraph
+    var msgDiv = document.createElement('div');
+    var paraElement = document.createElement('p');
+    paraElement.innerHTML = initialList[i];
+
+    // Add remove button
+    var remElement = document.createElement('button');
+    remElement.innerHTML = "Remove";
+    remElement.setAttribute("onclick", "removeMsg(" + listElement.id + ")");
+
+    msgDiv.appendChild(paraElement);
+    msgDiv.appendChild(remElement);
+    listElement.appendChild(msgDiv);
+    list.appendChild(listElement);
+
+    msgId++;
+  }
+}
+
+function post() {
+  var newMsg = document.getElementById("text-box").value;
+  if (newMsg) {
+    // Create list element
+    var listElement = document.createElement('li');
+    listElement.setAttribute("id", msgId);
+
+    // Create new div and add new paragraph
+    var msgDiv = document.createElement('div');
+    var paraElement = document.createElement('p');
+    paraElement.innerHTML = newMsg;
+
+    // Add remove button
+    var remElement = document.createElement('button');
+    remElement.innerHTML = "Remove";
+    remElement.setAttribute("onclick", "removeMsg(" + listElement.id + ")");
+
+    msgDiv.appendChild(paraElement);
+    msgDiv.appendChild(remElement);
+    listElement.appendChild(msgDiv);
+    list.appendChild(listElement);
+
+    // Clear text box
+    document.getElementById("text-box").value = '';
+
+    msgId++;
+  }
+}
+
+function clearAll() {
+  list.innerHTML = '';
+}
+
+function removeMsg(message) {
+  var div = document.getElementById(message);
+  list.removeChild(div);
+}
